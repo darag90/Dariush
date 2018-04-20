0f3b8cb05e6609fe32e0ad9bfe311ef9805982f0
Dariush <darag@kth.se>
2018-04-20 14:54:49 +0200

    test

---
+++ seminar3/src/View.java
@@ -0,0 +1,16 @@
+package view;
+
+import controller.Controller;
+
+public class View {
+    private Controller contr;
+
+    public View(Controller contr) {
+        this.contr = contr;
+    }
+
+    public void sampleExecution() {
+        contr.startSale();
+    }
+}
+
