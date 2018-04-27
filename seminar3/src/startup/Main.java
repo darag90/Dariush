d8492d3ee81c2d277f81cff5bf182b152d25230c
Simon Lagerqvist <simlag@kth.se>
2018-04-27 14:45:17 +0200

    Merge branch 'master' of https://github.com/darag90/Dariush
    # Conflicts:
    #	seminar3/src/customerRegister/CustomerRegister.java
    #	seminar3/src/itemList/ItemList.java
    
    vad: implemeterat enterItemId
    varför: --

--- seminar3/src/startup/Main.java
+++ seminar3/src/startup/Main.java
@@ -13,11 +13,15 @@ public class Main
 
     public static void main(String[] args)
     {
+        /*denna metod startar programmet och delar ut nödvöndiga referenser*/
+
         Printer printer = new Printer();
         PaymentController paymentController = new PaymentController(printer);
         ExternalSystem externalSystem = new ExternalSystem(printer);
         Controller contr = new Controller(externalSystem, paymentController);
         View view = new View(contr);
+
+        //starta exempel exikveringen av programmet
         view.sampleExecution();
     }
 }
