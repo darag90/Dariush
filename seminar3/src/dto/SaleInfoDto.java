417e9b6a0446695dbc295e4bbfb2d92afa00a0c7
Simon Lagerqvist <simlag@kth.se>
2018-04-30 13:55:29 +0200

    Merge branch 'master' of https://github.com/darag90/Dariush
    # Conflicts:
    #	seminar3/src/customerRegister/CustomerRegister.java
    #	seminar3/src/itemList/ItemList.java
    
     changes

--- seminar3/src/dto/SaleInfoDto.java
+++ seminar3/src/dto/SaleInfoDto.java
@@ -1,11 +1,14 @@
 package dto;
 
+import model.Item;//@todo item borde evntuelt flyttas till ite dto då vi får importer fram och tillbaka
+
 public class SaleInfoDto
 {
+    private int totalPrice;
+    private Item item;
+
     public SaleInfoDto()
     {
 
     }
-
-
 }
