ba17ae1cd760a9085fd7f6724bf65126de0cb963
Dariush <darag@kth.se>
2018-05-06 18:44:57 +0200

    Vad: skrev java dok varför: för att tydlig göra för andra som ska läsa koden

--- seminar3/src/se/kth/iv1201/pos/dbhandler/InventorySystem.java
+++ seminar3/src/se/kth/iv1201/pos/dbhandler/InventorySystem.java
@@ -14,15 +14,15 @@ kommer varoran vara inlagda som Item objekt från början.
 
     /**
      * Denna metod anger då item id, costnaden, och dess namne
+     * som exemel görs följande varor
+     * -gurka: id 1234
+     * -banan: id 5678
+     * -tandkräm id:1357
      */
 
     public InventorySystem()
     {
-        /*som exemel görs följande varor
-        -gurka: id 1234
-        -banan: id 5678
-        -tandkräm id:1357
-         */
+
         this.itemsInInventory.put( 1234, new Item(1234,20,"Gurka ekologisk") );
         this.itemsInInventory.put( 5678, new Item(1234,40,"Banan ica basic") );
         this.itemsInInventory.put( 1357, new Item(1234,60,"tandkräm oral-b") );
