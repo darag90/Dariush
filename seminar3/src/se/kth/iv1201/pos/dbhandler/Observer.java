48b075dfd615114984070f18c964b5f04e8c1f7f
Dariush <darag@kth.se>
2018-05-17 10:16:00 +0200

    Vad: interface

--- seminar3/src/se/kth/iv1201/pos/dbhandler/Observer.java
+++ seminar3/src/se/kth/iv1201/pos/dbhandler/Observer.java
@@ -1,5 +1,13 @@
 package se.kth.iv1201.pos.dbhandler;
 
+
+import se.kth.iv1201.pos.model.Sale;
+
 public interface Observer
 {
+    /**
+     * det kommer att kalla på sale när en försäljning sker
+     * @param sale när en försäljning genomförs
+     */
+    void notify(Sale sale);
 }
