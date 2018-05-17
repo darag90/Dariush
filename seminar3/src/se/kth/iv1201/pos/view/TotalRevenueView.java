f6204b60fa3684831d71d8b49f8b7895807f3a0f
Dariush <darag@kth.se>
2018-05-17 10:17:28 +0200

    kod

--- seminar3/src/se/kth/iv1201/pos/view/TotalRevenueView.java
+++ seminar3/src/se/kth/iv1201/pos/view/TotalRevenueView.java
@@ -1,5 +1,18 @@
+
+
 package se.kth.iv1201.pos.view;
 
-public class TotalRevenueView
-{
+import se.kth.iv1201.pos.dbhandler.Observer;
+import se.kth.iv1201.pos.model.Sale;
+
+public class TotalRevenueView implements Observer {
+    private int invalidItem = 0;
+    private int availableItem = 0;
+
+
+    public void notify(Sale sale){
+
+    }
+
+
 }
